76c6a0076a78c01fc32c75d6392e967667f2db7a
msroh <happywerrior@gmail.com>
2024-05-10 16:46:23 +0900

    first commit

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter, Lusitana, Noto_Sans_KR } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+
+export const noto_sans_kr = Noto_Sans_KR({subsets: ['latin']});
+
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin']});
